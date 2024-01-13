7a884cf1c4fad18efb13fe761a9c5639af90d137
Steve-omush <steveprogram60@gmail.com>
2024-01-13 11:26:26 -0800

    Task 4 Revised

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     modified_list = my_list.copy()
 
     for i in range(len(modified_list)):
