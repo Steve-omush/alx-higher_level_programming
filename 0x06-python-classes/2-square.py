25255109b51a9752aedc5dc24a3ab0061c595973
Steve-omush <steveprogram60@gmail.com>
2024-01-17 01:53:25 -0800

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Define Class Square."""
+
+
+class Square:
+    """Square class with a private size attribute."""
+    def __ini__(self, size=0):
+        """Initializes a new Square Instance.
+
+        Args:
+            size (int): Size of square. Default is 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
