c3a25fb85df3b7cb097f707c0e3118ab10ba1f36
Steve-omush <steveprogram60@gmail.com>
2024-01-23 05:11:47 -0800

    task 2 Revised 2.1

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-
-"""Define a function that prints a string."""
-
-
 def say_my_name(first_name, last_name=""):
-    """Prints the full name
-
-    Args:
-        first_name (str): first name passed. must be a string.
-        last_name (str): last name.
-    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
